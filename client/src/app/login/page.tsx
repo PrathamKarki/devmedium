6924d677abac00f6c5cde60e14335d0b5b87b998
pratham karki <prathamkarki21@gmail.com>
2025-07-28 14:09:20 +0545

    installation of shadcn components and lucide react icons

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,18 +1,20 @@
 'use client';
 import React from 'react'
 import { useState } from 'react';
+import {  Card, CardHeader, CardFooter, CardTitle,  CardAction,  CardDescription, CardContent, } from '@/components/ui/card';
+import { Label } from '@/components/ui/label';
 
 const Login = () => {
   const [showpassword, setShowPassword] = useState(false);
 
   return (
-      <main className='container flex items-center justify-center min-h-screen  '>
+      <main className=' flex items-center justify-center min-h-screen  bg-gray-100 '>
       
-          <div className="content shadow-xl px-6 py-4 ">
+          <div className="bg-red-100 w-full max-w-md p-8 rounded shadow-md">
 
             <div className='mb-5'>
               <h2 className='text-xl text-center p-2 font-bold'>Sign In</h2>
-              <p className='text-gray-600 text-lg'>Enter your credentials to access your account</p>
+              <p className='text-gray-600 text-lg text-center'>Enter your credentials to access your account</p>
             </div>
           
               <div className="input_field flex flex-col mt-5 mb-5">
