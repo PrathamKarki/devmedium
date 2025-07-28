5e9e59fff3fe097c6081f4c2bf52a4aad5ed4001
pratham karki <prathamkarki21@gmail.com>
2025-07-28 11:52:20 +0545

    created responsive navbar for homepage

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,28 @@
+'use client';
+import React from "react";
+import { Fragment } from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+const  Navbar = () => {
+    return(
+        <Fragment>
+            <header className="border-b-1">
+              <div className="w-[80%] max-w-7xl mx-auto">
+                <nav className="flex items-center justify-between">
+                     <Image src='/medium_logo.png' alt="logo" width={140} height={20}/>
+                    <ul className="flex gap-10 justify-between text-sm text-[#242424]">
+                        <li className="hidden md:block"><Link href='/'>Our Story</Link></li>
+                        <li className="hidden md:block"><Link href="/">Write</Link></li>
+                        <li><Link href="/"> Sign in</Link></li>
+                        <li><Link href="/">Register</Link></li>
+                    </ul>
+                </nav>
+                </div>
+            </header>
+
+        </Fragment>
+    )
+}
+
+export default Navbar;
